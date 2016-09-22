735ea556088752da2f003277fdc6021f373f7ac5
yang <yangkang@liveneeq.com>
2016-09-22 16:47:25 +0800

    Add mruby demo

--- README.md
+++ README.md
@@ -7,4 +7,16 @@ git submodule init
 git submodule init
 
 git submodule foreach git pull
+
+cd mruby
+make test
+
+vim ~/demo.rb
+```
+
+### demo.rb
+
+```ruby
+puts "hello world form demo.rb"
+MyModule.my_c_method('NSAlert')
 ```
--- huby/AppDelegate.m
+++ huby/AppDelegate.m
@@ -7,6 +7,34 @@
 //
 
 #import "AppDelegate.h"
+#include "mruby.h"
+#include "mruby/compile.h"
+#include <stdlib.h>
+#include <stdio.h>
+#import <objc/runtime.h>
+
+// https://developer.apple.com/reference/objectivec/1657527-objective_c_runtime
+// https://github.com/mruby/mruby/blob/master/src/class.c#L437
+// https://github.com/skandhas/mruby-core-ext/blob/master/src/core_ext_hash.c
+static mrb_value my_c_method(mrb_state *mrb, mrb_value self)
+{
+    puts("Called my C method");
+    char *class_name;
+    // Extract a value
+    mrb_get_args(mrb, "z", &class_name);
+    NSLog(@"class_name is %s", class_name);
+    Class theClass = NSClassFromString(@"NSAlert");
+    id alert = [[theClass alloc] init];
+    // NSAlert *alert = [NSAlert new];
+    [alert addButtonWithTitle:@"确定"];
+    [alert addButtonWithTitle:@"取消"];
+    [alert setMessageText:@"确定删除输入文本?"];
+    [alert setInformativeText:@"如果确定删除，删除的文本不能再找回!"];
+    [alert setAlertStyle:NSWarningAlertStyle];
+    [alert runModal];
+    
+    return mrb_nil_value();
+}
 
 @interface AppDelegate ()
 
@@ -16,7 +44,20 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    
+    mrb_state *mrb = mrb_open();
+    if (!mrb) { /* handle error */ }
+    mrb_load_string(mrb, "puts 'hello world form mrb_load_string'");
+    
+    struct RClass *mymodule = mrb_define_module(mrb, "MyModule");
+    mrb_define_class_method(mrb, mymodule, "my_c_method", my_c_method, MRB_ARGS_NONE());
+    NSString *homeDir = NSHomeDirectory();
+    NSString *rbpath = [homeDir stringByAppendingPathComponent:@"demo.rb"];
+    NSLog(@"rb path is %@", rbpath);
+    FILE *f = fopen([rbpath UTF8String], "r");
+    mrb_load_file(mrb, f);
+    
+    mrb_close(mrb);
 }
 
 
