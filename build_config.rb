d4c0a741af2cf54a3008ad0e6bb9bcd75c97bf6a
yang <yangkang@liveneeq.com>
2016-09-26 10:30:38 +0800

    Add libffi

--- build_config.rb
+++ build_config.rb
@@ -12,39 +12,24 @@ MRuby::Build.new do |conf|
 
   # ============================================================================
   # custom mrbgems
-  # mruby-polarssl
-  # conf.gem github: 'iij/mruby-io'
-  # conf.gem github: 'iij/mruby-socket'
-  # conf.gem github: 'iij/mruby-mtest'
-  # conf.gem github: 'luisbebop/mruby-polarssl'
-
   conf.gem github: 'iij/mruby-digest' # no dependency
-  # conf.gem github: 'qtkmz/mruby-digest-ffi'
 
-  # conf.gem github: 'take-cheeze/mruby-cfunc', branch: 'fix_for_latest_2015_05_29' do |g|
-  #   # g.use_pkg_config # use pkg-config for libffi linking
-  #   g.download_libffi # download and link latest libffi
-  #
-  #   # if your libffi is installed in a non standard path
-  #   # g.cc.include_paths << '[...]/include'
-  #   # g.linker.library_paths << '[...]/lib'
-  # end
+  # libffi
+  conf.gem "#{root}/mrbgems/mruby-eval"
+  conf.gem github: 'fundamental/mruby-cfunc', branch: 'master' do |g|
+    g.download_libffi
+  end
+  conf.gem "#{root}/mrbgems/mruby-struct"
+  conf.gem github: 'schmurfy/mruby-rubyffi-compat'
 
   # conf.gem github: 'mattn/mruby-curl'
 
   conf.gem github: 'FlowerWrong/mruby-crypto' # no dependency
 
-  # mruby-secure-random
-  # conf.gem github: 'iij/mruby-pack'
-  # conf.gem github: 'monochromegane/mruby-secure-random'
-
   # mruby-require
   conf.gem github: 'iij/mruby-dir'
   conf.gem github: 'iij/mruby-tempfile'
   conf.gem github: 'iij/mruby-require'
-
-  # conf.gem github: 'Asmod4n/mruby-tls'
-  # conf.gem github: 'iij/mruby-tls-openssl'
 
   # mruby-libuv
   # conf.cc.flags << '-DMRB_INT64'
