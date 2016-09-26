5ec53f65bf3c4747e279636a03ba2aeff139aad2
yang <yangkang@liveneeq.com>
2016-09-26 15:31:20 +0800

    Add some gem

--- build_config.rb
+++ build_config.rb
@@ -19,8 +19,8 @@ MRuby::Build.new do |conf|
   # conf.gem github: 'baldowl/mruby-bcrypt' # FIXME mruby-onig-regexp some big bug
   conf.gem github: 'mattn/mruby-base64' # worked
   conf.gem github: 'mattn/mruby-md5' # worked
-  # conf.gem github: 'mattn/mruby-sha1'
-  # conf.gem github: 'h2so5/mruby-sha2'
+  conf.gem github: 'mattn/mruby-sha1' # worked
+  # conf.gem github: 'h2so5/mruby-sha2' comflict with mruby-crypto
   # conf.gem github: 'Asmod4n/mruby-libsodium' # TODO
 
   # ============================================================================
@@ -30,10 +30,10 @@ MRuby::Build.new do |conf|
   conf.gem github: 'iij/mruby-ipaddr' # worked
   # conf.gem github: 'carsonmcdonald/mruby-markdown' # TODO
 
-  # conf.gem github: 'h2so5/mruby-tinyxml2' # TODO compile failed
+  # conf.gem github: 'h2so5/mruby-tinyxml2' # TODO duplicate symbols
   conf.gem github: 'jbreeden/mruby-sqlite' # worked
   conf.gem github: 'AndrewBelt/mruby-yaml' # worked
-  # conf.gem github: 'jbreeden/mruby-zlib'
+  # conf.gem github: 'jbreeden/mruby-zlib' error: implicit declaration
 
   # ============================================================================
   # mruby-require
@@ -44,12 +44,12 @@ MRuby::Build.new do |conf|
   conf.gem github: 'iij/mruby-io'
   conf.gem github: 'iij/mruby-pack'
   conf.gem github: 'iij/mruby-socket'
-  # conf.gem github: 'mattn/mruby-thread' # TODO
+  conf.gem github: 'mattn/mruby-thread' # worked
 
   conf.gem github: 'suzukaze/mruby-msgpack' # worked
   conf.gem github: 'ksss/mruby-ostruct' # worked
 
-  # conf.gem github: 'iij/mruby-regexp-pcre' # TODO
+  conf.gem github: 'iij/mruby-regexp-pcre' # worked
 
   conf.gem github: 'matsumoto-r/mruby-sleep' # worked
   conf.gem github: 'ascaridol/mruby-statemachine' # worked
@@ -86,7 +86,7 @@ MRuby::Build.new do |conf|
   conf.gem github: 'schmurfy/mruby-rubyffi-compat'
 
 
-  conf.gem github: 'kjunichi/mruby-mrmagick' # TODO
+  # conf.gem github: 'kjunichi/mruby-mrmagick' # TODO
 
   # ============================================================================
 end
